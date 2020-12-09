9749a7501c53a882f0e77f932366c75faf2b1934
misatol8k <misatolkurihara@gmail.com>
2020-12-09 16:36:07 +0900

    ステップ３機能を追加しました

--- sample.rb
+++ sample.rb
@@ -1,8 +1,8 @@
-# require 'byebug'
+require 'byebug'
 # このコードをコピペしてrubyファイルに貼り付け、そのファイルをirbでrequireして実行しましょう。
 # 例
 # irb
-# require '/Users/mlkuri/workspace/sample.rb'
+# require '/Users/mlkuri/workspace/vm/sample.rb'
 # （↑のパスは、自動販売機ファイルが入っているパスを指定する）
 # 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
 # vm = VendingMachine.new
@@ -20,7 +20,10 @@ class VendingMachine
   # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize
     # 最初の自動販売機に入っている金額は0円
+    @sale = 0
     @slot_money = 0
+    @coke = [{name:"coke",price:120},{name:"coke",price:120}]
+# 在庫を追加
   end
   # 投入金額の総計を取得できる。
   def current_slot_money
@@ -33,10 +36,9 @@ class VendingMachine
     # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
     # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
     if MONEY.include?(money)
-
     return false unless MONEY.include?(money)
     # 自動販売機にお金を入れる
-
+    @slot_money += money
     end
   end
   # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
@@ -46,23 +48,21 @@ class VendingMachine
     # 自動販売機に入っているお金を0円に戻す
     @slot_money = 0
   end
-end
-# class Stock
-#   stock = {price:, name:}
-#   coke =  [{ 名前:コーラ,値段: 120 },{ 名前:コーラ,値段: 120 },{ 名前:コーラ,値段: 120 }{ 名前:コーラ,値段: 120 }{ 名前:コーラ,値段: 120 }]
-#   def initialize()
-#     @stock = stock
-#   end
-# end
-# @stock = Stock.new()
-#
-class Stock
-  def initialize
-    @coke = [{name:"coke",price:120},{name:"coke",price:120}]
+  def stock
+    @stock_coke = @coke.size
   end
-  def stock_list
-    puts "在庫は#{initialize.size}個です"
+  def purchase
+    # byebug
+    if @slot_money >= 120 && @coke.size >= 1
+      @sale += 120
+      @coke  = @coke.shift #配列から最初の１つを取り出して消す処理をかく
+      @slot_money -= 120
+    else
+      return false
+    end
   end
 end
-stock = Stock.new
-stock.stock_list
+
+vm = VendingMachine.new
+p vm.slot_money(1000)
+p vm.purchase
